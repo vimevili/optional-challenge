b1b6d630d6d7ac3dec441532d33a73d3b6fbafe8
Vinicius Meirelles <vinicius_meirelless@hotmail.com>
2023-07-10 18:17:10 -0300

    add code for transform wind speed from meters/sec to km/sec

--- src/components/weather/WeatherCard.tsx
+++ src/components/weather/WeatherCard.tsx
@@ -1,5 +1,6 @@
 
 const WeatherCard = ({info, value}) => {
+  const windInKm = Math.round(value * 3.6)
   return (
     <div className="grid grid-cols-[auto_2fr_1fr] gap-[8px] bg-white-36 items-start px-[20px] py-[15px] rounded-[10px] max-w-[90%] mx-auto my-0 text-bs">
       <img src={info==='Wind' ? 'src/assets/img/wind.svg' : 'src/assets/img/humidity.svg'} alt="" className="block"/>
@@ -7,7 +8,7 @@ const WeatherCard = ({info, value}) => {
         {info}
         </p>
        <p className="text-end">
-        {info==='Wind' ? value+'km/h' : value+'%'}
+        {info==='Wind' ? windInKm+'km/h' : value+'%'}
         </p>
     </div>
   )
